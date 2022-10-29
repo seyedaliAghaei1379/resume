031f1090731454d76a8ea5d989e7f9f0de6d29bf
ali <seyedali1379aghaei@gmail.com>
2022-10-29 02:20:38 -0700

    testimonial

--- assets/js/main.js
+++ assets/js/main.js
@@ -33,10 +33,35 @@ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 
 /*=============== SWIPER PROJECTS ===============*/
-
+let swiperProjects = new Swiper(".projects__container", {
+    loop : true,
+    spaceBetween : 24,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+    breakpoints: {
+        1200: {
+          slidesPerView: 2,
+          spaceBetween: -56,
+        },
+      },
+    mousewheel: true,
+    keyboard: true,
+  });
 
 /*=============== SWIPER TESTIMONIAL ===============*/
-
+let swiperTestimonial = new Swiper(".testimonial__container", {
+    grabCursor : true,
+    
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
 
 /*=============== EMAIL JS ===============*/
 
